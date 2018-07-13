a9e6b39e2bb09f57bf713051e41ec3cd1bc8cfa7
pegdwende <rodrigue2014@yahoo.fr>
2018-07-13 07:00:48 -0500

    initial commit with dummy authentification

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by pegdwende on 7/13/2018.
+ */
+
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.post('/api/auth',(req, res)=>{
+  res.status(400).json({errors:{global:"Invalid credentials"}});
+ });
+
+app.get('/*', (req, res)=>{
+ res.sendFile(path.join(__dirname,'index.html'))
+}
+);
+
+app.listen(8080,()=>console.log('Running on local 8080'));
